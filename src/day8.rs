89888e970501ebcb87a622e5dbc6860229eb92ca
Thomas Kendrick <tom@tkendrick.com>
2022-12-08 13:07:42 +0000

    refactor day 8

--- src/day8.rs
+++ src/day8.rs
@@ -1,3 +1,5 @@
+use std::ops::Range;
+
 #[derive(Debug, PartialEq)]
 struct Tree {
     x: usize,
@@ -6,59 +8,48 @@ struct Tree {
 }
 
 impl Tree {
+    fn is_blocked_dir(&self, map: &Map, range: Range<usize>, vert: bool) -> bool {
+        range.into_iter().any(|i| {
+            if vert {
+                map[i][self.x].height >= self.height
+            } else {
+                map[self.y][i].height >= self.height
+            }
+        })
+    }
+    fn score_dir(&self, map: &Map, iter: impl Iterator<Item = usize>, vert: bool) -> usize {
+        let mut score = 0;
+        for i in iter {
+            let cond = if vert {
+                map[i][self.x].height >= self.height
+            } else {
+                map[self.y][i].height >= self.height
+            };
+            score += 1;
+            if cond {
+                break;
+            }
+        }
+        score
+    }
     fn new(x: usize, y: usize, height: u32) -> Self {
         Self { x, y, height }
     }
     fn check_visible(&self, map: &Map) -> bool {
-        let m_height = map.len();
-        let m_width = map[0].len();
-        let n_blocked = (0..self.y)
-            .into_iter()
-            .any(|i| map[i][self.x].height >= self.height);
-        let s_blocked = (self.y + 1..m_height)
-            .into_iter()
-            .any(|i| map[i][self.x].height >= self.height);
-        let e_blocked = (self.x + 1..m_width)
-            .into_iter()
-            .any(|i| map[self.y][i].height >= self.height);
-        let w_blocked = (0..self.x)
-            .into_iter()
-            .any(|i| map[self.y][i].height >= self.height);
+        let n_blocked = self.is_blocked_dir(map, 0..self.y, true);
+        let s_blocked = self.is_blocked_dir(map, self.y + 1..map.len(), true);
+        let e_blocked = self.is_blocked_dir(map, self.x + 1..map[0].len(), false);
+        let w_blocked = self.is_blocked_dir(map, 0..self.x, false);
         !n_blocked || !s_blocked || !e_blocked || !w_blocked
     }
     fn calc_score(&self, map: &Map) -> usize {
-        let (mut ns, mut ss, mut es, mut ws) = (0, 0, 0, 0);
         let m_height = map.len();
         let m_width = map[0].len();
-        // Check north
-        for i in (0..self.y).rev() {
-            ns += 1;
-            if map[i][self.x].height >= self.height {
-                break;
-            }
-        }
-        // Check south
-        for i in self.y + 1..m_height {
-            ss += 1;
-            if map[i][self.x].height >= self.height {
-                break;
-            }
-        }
-        // Check east
-        for i in self.x + 1..m_width {
-            es += 1;
-            if map[self.y][i].height >= self.height {
-                break;
-            }
-        }
-        // Check west
-        for i in (0..self.x).rev() {
-            ws += 1;
-            if map[self.y][i].height >= self.height {
-                break;
-            }
-        }
-        ns * ss * es * ws 
+        let ns = self.score_dir(map, (0..self.y).rev(), true);
+        let ss = self.score_dir(map, self.y + 1..m_height, true);
+        let es = self.score_dir(map, self.x + 1..m_width, false);
+        let ws = self.score_dir(map, (0..self.x).rev(), false);
+        ns * ss * es * ws
     }
 }
 
